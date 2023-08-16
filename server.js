da444ae47cf2b5a89af8de1d6d617d415a6b4efa
Kris Kurzawa <kurzawa1@yahoo.com>
2023-08-16 17:26:12 -0400

    updated server

--- server.js
+++ server.js
@@ -3,25 +3,20 @@ const path = require('path');
 const app = express();
 const notesData = require('./db/db.json');
 const fs = require('fs');
-const uniqid = require('uniqid');
+const { v4: uuidv4 } = require('uuid');
+const userId = uuidv4();
 const { readFromFile, writeToFile, readAndAppend } = require('./helpers/fsUtils');
 
-
-
-
 const PORT = 3001;
-
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// GET Route for homepage
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for notes page
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
@@ -31,22 +26,34 @@ app.get('/api/notes', (req, res) => {
         .then((dataBuffer) => {
             let data = dataBuffer.toString();
             res.json(JSON.parse(data))
-        })
+        });
+});
 
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((notes) => {
+            const result = notes.filter((note) => note.id !== noteId);
+            writeToFile('./db/db.json', result);
+            res.send();
+        });
 });
 
 app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+        readAndAppend(newNote, './db/db.json')
+        res.send();
+    } else {
 
-    readAndAppend(req.body, './db/db.json')
-    res.send();
-})
-
-app.delete('/api/notes/id', (req, res) => {
-
-})
-
-
-
+    };
+});
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
